dbf3a093422b84620cc7c5b46bffcf34c4556772
Sudais1111 <154426154+Sudais1111@users.noreply.github.com>
2024-01-20 13:42:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923158079733', 'sudais', true],
+  ['923158079733', 'ahmad', true], 
   [''] 
 ] //Number of owners
 
